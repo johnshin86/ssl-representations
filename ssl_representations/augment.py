780bd5d9d53f6a01231bb48e8c12a4a6e8d55f1a
John Shin <johnyoungshin@gmail.com>
2022-12-27 17:08:54 -0500

    Cleaning augment classes, making parameters attributes, made previous weak aug_strength medium, and added new weak aug_strength.

--- ssl_representations/augment.py
+++ ssl_representations/augment.py
@@ -8,13 +8,15 @@ class VOC_preprocess:
     """
     def __init__(self, data_preprocess: str):
 
-        mean = torch.tensor([0.4570, 0.4382, 0.4062])
-        std = torch.tensor([0.2345, 0.2305, 0.2353])
+        self.data_preprocess = data_preprocess
+        #Mean and std of the dataset computed with (224,224) center cropping.
+        self.mean = torch.tensor([0.4570, 0.4382, 0.4062])
+        self.std = torch.tensor([0.2345, 0.2305, 0.2353])
 
         if data_preprocess == "normalize":
             self.transforms = T.Compose([
                     T.ToTensor(),
-                    T.Normalize(mean, std),
+                    T.Normalize(self.mean, self.std),
                     T.Resize((224, 224))
             ])
         else:
@@ -22,7 +24,6 @@ class VOC_preprocess:
                     T.ToTensor(),
                     T.Resize((224, 224))
             ])
-
 
     def __call__(self, img):
         return self.transforms(img)
@@ -32,13 +33,24 @@ class VOC_augment:
     training for non-contrastive self-supervised learning methods. 
     """
     def __init__(self, aug_policy: str, aug_strength: str):
+
+        self.aug_policy = aug_policy
+        self.aug_strength = aug_strength
+
         self.RandomJitter = T.RandomApply([T.ColorJitter(0.4, 0.4, 0.2, 0.1)], p = 0.8)
         self.RandomGrayscale = T.RandomApply([T.Grayscale(num_output_channels = 3)], p = 0.2)
         self.RandomGaussian = T.RandomApply([T.GaussianBlur(kernel_size = 3)], p = 0.5)
 
-        if aug_policy == 'standard':
+        if self.aug_policy == 'standard':
 
-            if aug_strength == 'weak':
+
+            if self.aug_strength == 'weak':
+
+                self.transforms = T.Compose([
+                    self.RandomGaussian,
+                    ])
+
+            elif self.aug_strength == 'medium':
 
                 self.transforms = T.Compose([
                     T.RandomHorizontalFlip(p = 0.5),
@@ -46,7 +58,8 @@ class VOC_augment:
                     self.RandomGrayscale,
                     self.RandomGaussian,
                     ])
-            elif aug_strength == 'strong':
+
+            elif self.aug_strength == 'strong':
                 self.transforms = T.Compose([
                     T.RandomResizedCrop(size = (224,224), scale = (0.05, 1.0))
                     T.RandomHorizontalFlip(p = 0.5),
