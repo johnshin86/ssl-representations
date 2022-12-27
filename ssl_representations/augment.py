18605cf37dffd0b4f297a1a6415ab370bd593b51
John Shin <johnyoungshin@gmail.com>
2022-12-27 15:37:05 -0500

    Converting aug policies to VOC.

--- ssl_representations/augment.py
+++ ssl_representations/augment.py
@@ -8,8 +8,13 @@ class VOC_preprocess:
     """
     def __init__(self, data_preprocess: str):
 
+        mean = torch.tensor([0.4570, 0.4382, 0.4062])
+        std = torch.tensor([0.2345, 0.2305, 0.2353])
+
         self.transforms = T.Compose([
                 T.ToTensor(),
+                T.Normalize(mean, std),
+                T.Resize((224, 224))
         ])
 
     def __call__(self, img):
@@ -19,24 +24,38 @@ class VOC_augment:
     r"""The augment class for the PASCAL VOC 2012 dataset. This is designed to be used to augment the dataset during
     training for non-contrastive self-supervised learning methods. 
     """
-    def __init__(self, data_augmentation: str):
+    def __init__(self, aug_policy: str, aug_strength: str):
         self.RandomJitter = T.RandomApply([T.ColorJitter(0.4, 0.4, 0.2, 0.1)], p = 0.8)
         self.RandomGrayscale = T.RandomApply([T.Grayscale(num_output_channels = 3)], p = 0.2)
         self.RandomGaussian = T.RandomApply([T.GaussianBlur(kernel_size = 3)], p = 0.5)
 
-        if data_augmentation == 'vicreg':
+        if aug_policy == 'vicreg':
 
-            self.transforms = T.Compose([
+            if aug_strength == 'weak':
+
+                self.transforms = T.Compose([
                     T.RandomHorizontalFlip(p = 0.5),
                     self.RandomJitter,
                     self.RandomGrayscale,
                     self.RandomGaussian,
-                    #T.RandomSolarize(p = 0.1),
                     ])
+            elif aug_strength == 'strong':
+                self.transforms = T.Compose([
+                    T.RandomResizedCrop(size = (224,224), scale = (0.05, 1.0))
+                    T.RandomHorizontalFlip(p = 0.5),
+                    self.RandomJitter,
+                    self.RandomGrayscale,
+                    self.RandomGaussian,
+                    T.RandomSolarize(p = 0.1),
+                    ])
+
+            else:
+                raise ValueError(f'Unknown augmentation strength "{aug_strength}"')
+
                 
 
         else:
-            raise ValueError(f'Unknown data augmentation policy "{data_augmentation}"')
+            raise ValueError(f'Unknown data augmentation policy "{aug_policy}"')
 
     def __call__(self, img):
         return self.transforms(img)
