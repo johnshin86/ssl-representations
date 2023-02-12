6a13b291654b17cd7352ba52325b8e9addf235dd
John Shin <johnyoungshin@gmail.com>
2023-02-11 21:40:12 -0500

    Working on InfoNCE.

--- ssl_representations/infonce.py
+++ ssl_representations/infonce.py
@@ -11,6 +11,18 @@ class InfoNCE(_Loss):
 		super(InfoNCE, self).__init__(size_average, reduce, reduction)
 
 	def forward(self, z_a: Tensor, z_b: Tensor) -> Tensor:
+		# Build similarity matrix
+
+		z = torch.cat( (z_a, z_b), dim = 0) # 2N x F
+
+		z_norm = torch.linalg.norm(z, ord = 2, dim = 1) # Norm over F
+
+		z_normalized = z / z_norm[:, None]
+
+		similarity = z_normalized @ z_normalized.T 
+
+		#masking?
+
 		return 
 
 
