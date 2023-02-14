e4966c6c0a8faccf7297f22753711b66a65c55e5
John Shin <johnyoungshin@gmail.com>
2023-02-13 19:09:35 -0500

    Moving off_diagonal to internal class method.

--- ssl_representations/barlow_twins.py
+++ ssl_representations/barlow_twins.py
@@ -38,13 +38,13 @@ class BarlowTwinsLoss(_Loss):
         torch.distributed.all_reduce(c)
 
         on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
-        off_diag = off_diagonal(c).pow_(2).sum()
+        off_diag = _off_diagonal(c).pow_(2).sum()
 
         return on_diag, off_diag
 
-def off_diagonal(x):
-    # return a flattened view of the off-diagonal elements of a square matrix
-    n, m = x.shape
-    assert n == m
-    return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+    def _off_diagonal(x):
+        # return a flattened view of the off-diagonal elements of a square matrix
+        n, m = x.shape
+        assert n == m
+        return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
 
