fa846b67667b46baa678b30e9285e813f561017b
John Shin <johnyoungshin@gmail.com>
2023-02-13 23:52:00 -0500

    Refactoring InfoNCE

--- ssl_representations/infonce.py
+++ ssl_representations/infonce.py
@@ -4,28 +4,5 @@ from torch.nn.modules.loss import _Loss
 from torch.nn.functional import relu
 
 
-class InfoNCE(_Loss):
-	r"""
-	"""
-	def __init__(self, size_average = None, reduce = None, reduction: str = 'mean') -> None:
-		super(InfoNCE, self).__init__(size_average, reduce, reduction)
-
-	def forward(self, z_a: Tensor, z_b: Tensor) -> Tensor:
-		# Build similarity matrix
-
-		z = torch.cat( (z_a, z_b), dim = 0) # 2N x F
-
-		z_norm = torch.linalg.norm(z, ord = 2, dim = 1) # Norm over F
-
-		# Is F.normalize faster?
-
-		z_normalized = z / z_norm[:, None]
-
-		similarity = z_normalized @ z_normalized.T 
-
-		#For a given i, need to mask the i'th entry in the normalization sum. 
-		#To do this, we mask the diagonal, then sum the row
-
-		return 
 
 
