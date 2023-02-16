13653a98255d28d2b482be8ba11502a7b3fdcfce
John Shin <johnyoungshin@gmail.com>
2023-02-16 18:43:23 -0500

    Removed batchnorm and lastdim.

--- ssl_representations/infonce.py
+++ ssl_representations/infonce.py
@@ -22,9 +22,6 @@ class InfoNCE(nn.Module):
         self.backbone = model
         self.projector = Projector(args, self.rep_dim)
 
-        # normalization layer for the representations z1 and z2
-        out_dim = list(map(int, args.mlp.split('-')))[-1]
-        self.bn = nn.BatchNorm1d(self.embedding_dim, affine=False)
 
     def forward(self, y1, y2):
         z1 = self.projector(self.backbone(y1))
