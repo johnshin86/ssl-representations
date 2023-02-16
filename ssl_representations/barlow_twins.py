6869b01edbeaa8a82aabcf6beb140409ee552298
John Shin <johnyoungshin@gmail.com>
2023-02-15 20:43:58 -0500

    Added coefficient for barlowtwins.

--- ssl_representations/barlow_twins.py
+++ ssl_representations/barlow_twins.py
@@ -30,6 +30,6 @@ class BarlowTwins(nn.Module):
 
         on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
         off_diag = off_diagonal(c).pow_(2).sum()
-        loss = on_diag + self.args.lambd * off_diag
+        loss = on_diag + self.args.coeffs * off_diag
         return loss
 
---
+++ ssl_representations/projector.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+
+def Projector(args, embedding):
+    mlp_spec = f"{embedding}-{args.mlp}"
+    layers = []
+    f = list(map(int, mlp_spec.split("-")))
+    for i in range(len(f) - 2):
+        layers.append(nn.Linear(f[i], f[i + 1]))
+        layers.append(nn.BatchNorm1d(f[i + 1]))
+        layers.append(nn.ReLU(True))
+    layers.append(nn.Linear(f[-2], f[-1], bias=False))
+    return nn.Sequential(*layers)
